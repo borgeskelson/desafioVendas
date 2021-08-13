363c01188c8ba10c17e98549fd14e9ba6c586fc2
Kelson Borges <kelson.borges@inema.ba.gov.br>
2021-08-12 23:23:58 -0300

    Ajustando modelos Usuario e Produto

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -2,14 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class Usuario extends Authenticatable
+class Usuario extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
     protected $table = "usuario";
     protected $primaryKey = "id";
